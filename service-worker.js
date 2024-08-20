100e137dba591ae00a6b2088aa727bca916c1971
semkooleg378 <semkooleg378@gmail.com>
2024-08-20 12:32:01 +0300

    files

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = 'ble-device-manager-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
